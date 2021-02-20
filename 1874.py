3bb5827a5e033761b5859cd25741cf4c44d7ca90
yujinHan97 <dbwlslek97@gmail.com>
2021-02-20 22:42:41 +0900

    스택수열

--- 1874.py
+++ 1874.py
@@ -7,35 +7,35 @@
 '''
 import sys
 n = int(input())
-seq = []
-op = []
+seq = [] # 수열을 저장할 리스트
+op = [] # 출력할 연산을 저장할 리스트
 stack = []
 for _ in range(n):
     seq.append(int(sys.stdin.readline()))
 
 start = 0
 for i in range(len(seq)):
-    if len(stack) == 0:
+    if len(stack) == 0: # 스택이 비어있으면 push
         for j in range(start+1, seq[i] + 1):
             stack.append(j)
-            start = stack[len(stack)-1]
+            start = stack[len(stack)-1] # 다음 push할때 시작 할 숫자를 저장하기 위한 변수
             op.append('+')
-        stack.pop()
+        stack.pop() # 마지막으로 push된 숫자는 수열의 숫자니까 pop
         op.append('-')
         continue
-    if seq[i] < seq[i-1]:
-        if seq[i] >= stack[len(stack)-1]:
+    if seq[i] < seq[i-1]: # 수열의 현재 숫자가 이전의 숫자보다 작으면 pop
+        if seq[i] >= stack[len(stack)-1]: # 수열의 현재 숫자가 스택의 top보다 작으면 pop할 수 없으니까, 크거나 같으면 pop
             stack.pop()
             op.append('-')
-        else:
+        else: # 수열의 현재 숫자가 스택 탑보다 작으면 pop을 할 수 없으므로, op연산 싹 다 지우고 종료
             op.clear()
             break
-    else:
-        for j in range(start + 1, seq[i] + 1):
+    else: # 수열의 현재 숫자가 수열의 이전 숫자보다 크면 그 만큼 push를 해줘야한다
+        for j in range(start + 1, seq[i] + 1): # 이전 push할 때 마지막으로 저장한 수보다 큰 수 부터 수열의 현재 수까지 push
             stack.append(j)
             start = stack[len(stack)-1]
             op.append('+')
-        stack.pop()
+        stack.pop() 
         op.append('-')
 
 if len(op) == 0:
