1d1249c160984e0b27acd930601bb3002262976f
Yujin <55319788+yujinHan97@users.noreply.github.com>
2021-06-24 11:14:31 +0900

    Update 타겟 넘버.py

--- 타겟 넘버.py
+++ 타겟 넘버.py
@@ -2,6 +2,7 @@
     이런 문제도 DFS/BFS로 풀 수 있다는 것 알기! 
     완전 탐색해도 시간 범위내에 나올수 있음
     맨 처음에 BFS 함수를 따로 빼서 했고, while deq 이게 맞나 싶엇는데 다시 풀어보기
+    (DFS, 다른 방법 또 있음 풀어보기)
 '''
 from collections import deque
 def solution(numbers, target):
