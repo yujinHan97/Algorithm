ba9c85e70e84765784cdadf008913ec73fed9186
Yujin <55319788+yujinHan97@users.noreply.github.com>
2021-07-11 11:16:31 +0900

    약수의 개수와 덧셈

--- 약수의 개수와 덧셈.py
+++ 약수의 개수와 덧셈.py
@@ -1,3 +1,18 @@
+'''
+    * 제곱수는 약수의 개수가 홀수개, 그 외는 모두 짝수개
+
+    따라서, isEven이라는 함수를 따로 정의하지 않고도 가능
+        if (i**0.5) == (int)(i**0.5) --> 제곱수 판별
+    
+    def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        if (i**0.5) == (int)(i**0.5): 
+            answer -= i
+        else:
+            answer += i
+    return answer
+'''
 def isEven(num):
     count = 0
     for i in range(1, num+1):
