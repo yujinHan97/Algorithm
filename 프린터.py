f0ec169f4d1c6670f79f4edf5f14ad8f6553d6b2
Yujin <55319788+yujinHan97@users.noreply.github.com>
2021-07-22 23:21:33 +0900

    프린터

--- 프린터.py
+++ 프린터.py
@@ -1,3 +1,21 @@
+# 다른 사람 풀이
+def solution(priorities, location):
+    answer = 0
+    # enumerate 사용법 알기!
+    dq = [(i, p) for i, p in enumerate(priorities)]
+    
+    while True:
+        cur = dq.pop(0)
+
+        # any 사용법 알기! 
+        if any(cur[1] < q[1] for q in dq):
+            dq.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+
+# 내 풀이
 from collections import deque
 def solution(priorities, location):
     answer = 0
