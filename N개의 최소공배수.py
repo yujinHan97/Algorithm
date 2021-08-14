2c73a517484914512e71997067d7c4fb77dfa8bf
Yujin <55319788+yujinHan97@users.noreply.github.com>
2021-08-14 16:34:21 +0900

    N개의 최소공배수 (다시풀기)

--- N개의 최소공배수.py
+++ N개의 최소공배수.py
@@ -4,12 +4,30 @@
     2. 이 점을 이용하여 배열을 정렬하고 2개씩 묶어서 최소공배수를 구하고,
        구한 그 최소공배수와 그 다음 요소의 최소공배수를 구하는 것을 반복
 '''      
+# 더 나은 풀이
+def getGCD(a, b):
+    if b == 0:
+        return a
+
+    return getGCD(b, a%b)
+
+def solution(arr):    
+    answer = 1
+    arr.sort(reverse = True)
+    
+    for i in range(len(arr)-1):
+        gcd = getGCD(arr[i], arr[i+1])
+        arr[i+1] = gcd * (arr[i] // gcd) * (arr[i+1] // gcd)
+
+    return arr[-1]
+
+# 내 풀이
 from collections import deque
 def getGCD(a, b):
     if b == 0:
         return a
 
-    if a >= b:
+    if a >= b: # 이미 내림차순 정렬 해서 if문 필요 X
         gcd = getGCD(b, a % b)
     else:
         a, b = b, a
