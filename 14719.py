e666eff774fc31cac16789d4b6dce7f2e7e51680
Yujin <55319788+yujinHan97@users.noreply.github.com>
2022-03-08 13:58:08 +0900

    빗물

--- 14719.py
+++ 14719.py
@@ -21,3 +21,32 @@ for i in range(1, W-1):
     
 print(count)
     
+---------------------------------------------------------------------------------------------------------
+'''
+    알고리즘: (직접 풀어서 맞춤 - 50분)
+    1. 2차원 세계의 블록을 graph로 만들어서 블록을 1로 초기화 
+    2. graph의 모든 칸 마다 0이면, 해당 col 기준으로 왼쪽 오른쪽에 1이 모두 있으면 빗물이 고일 수 있음
+'''
+import sys
+input = sys.stdin.readline
+
+h, w = map(int, input().split())
+heights = list(map(int, input().split()))
+
+graph = [[0] * w for _ in range(h)]
+for i in range(len(heights)):
+  for j in range(heights[i]):
+    graph[h-j-1][i] = 1
+
+answer = 0
+for i in range(h):
+  for j in range(w):
+    if j == 0 or j == w-1: # 가장 끝 칸들은 어차피 빗물이 고일 수 없으니까 제외
+      continue
+    if graph[i][j] == 0:
+      # 왼쪽과 오른쪽 모두 1이 있으면 == 블록이 양옆에 있으므로 빗물이 싸일 수 있음
+      if graph[i][:j].count(1) != 0 and graph[i][j+1:].count(1) != 0:
+        graph[i][j] = 2 # 빗물을 2로 표시 후, answer 증가
+        answer += 1
+        
+print(answer)
