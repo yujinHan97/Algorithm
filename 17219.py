53474b850747f3c9b36bd9327e12f0d111bcd2f7
Yujin <55319788+yujinHan97@users.noreply.github.com>
2022-04-04 22:22:42 +0900

    비밀번호 찾기

---
+++ 17219.py
@@ -0,0 +1,17 @@
+'''
+    알고리즘:
+    1. 중복된 사이트는 없으니까, 사전 자료형 사용
+    2. url에 해당하는 pwd 출력하면 된다
+'''
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+pwd_dict = {}
+for _ in range(n):
+  addr, pwd = map(str, input().split())
+  pwd_dict[addr] = pwd
+  
+for _ in range(m):
+  url = input().strip()
+  print(pwd_dict[url])
